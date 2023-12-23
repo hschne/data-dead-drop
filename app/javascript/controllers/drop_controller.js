be999dec4ccb1bfbd927d5f67a2a2c857f2e6f8a
hschne <hans.schnedlitz@gmail.com>
2023-12-23 19:54:06 +0100

    Fix drop

--- app/javascript/controllers/drop_controller.js
+++ app/javascript/controllers/drop_controller.js
@@ -1,7 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["text", "uploadIcon", "fileIcon"];
+  static targets = ["input", "text", "uploadIcon", "fileIcon"];
+
+  drop(event) {
+    event.preventDefault();
+    if (event.dataTransfer.items) {
+      const files = event.dataTransfer.items;
+      const file = files[0].getAsFile();
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(file);
+      this.inputTarget.files = dataTransfer.files;
+
+      this.textTarget.innerHTML = file.name;
+      this.uploadIconTarget.classList.toggle("hidden");
+      this.fileIconTarget.classList.toggle("hidden");
+    }
+  }
 
   display(event) {
     const file = event.target.files[0];
