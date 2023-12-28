eadb93dba20e2ac826aa48483839f1c2890a93fc
hschne <hans.schnedlitz@gmail.com>
2023-12-28 18:44:59 +0100

    Change font

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["Space Grotesk", ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
         brutal:
