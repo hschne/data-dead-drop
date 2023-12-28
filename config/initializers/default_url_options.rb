d7ccad2441d79329e6bcecffbbd1dc17e20f43cc
hschne <hans.schnedlitz@gmail.com>
2023-12-28 20:01:34 +0100

    Change url options, fix link

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 hosts = {
-  development: 'localhost:3000',
-  test: 'localhost:3000',
-  production: 'datadeaddrop.com'
+  development: 'http://localhost:3000',
+  test: 'http://localhost:3000',
+  production: 'https://datadeaddrop.com'
 }.freeze
 
 Rails.application.routes.default_url_options[:host] = hosts[Rails.env.to_sym]
